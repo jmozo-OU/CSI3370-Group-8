677538f7cc5cb1ddb72ec2032994ed4b2198f220
Richast <richard.pluta.12@gmail.com>
2021-01-31 19:37:07 +0000

    Sets up gradle project

---
+++ src/main/java/reverseuml/ReverseUmlApplication.java
@@ -0,0 +1,13 @@
+package reverseuml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseUmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reverseuml/ReverseUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package reverseuml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
