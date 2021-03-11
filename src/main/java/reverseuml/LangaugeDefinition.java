a7a224f8eaf910ffeed25183079be408793c4f24
NeighboM <Yelk11@users.noreply.github.com>
2021-03-11 16:32:25 -0500

    Create LangaugeDefinition.java

---
+++ src/main/java/reverseuml/LangaugeDefinition.java
@@ -0,0 +1,55 @@
+public class LangaugeDefinition {
+    private String languageName;
+    private String languageFileExtension;
+    private String classToken;
+    private String attributeToken;
+    private String methodToken;
+
+    public LangaugeDefinition(String langName, String languageFileExtension, String classToken, String attribToken, String methodToken){
+        this.languageName = langName;
+        this.languageFileExtension = languageFileExtension;
+        this.classToken = classToken;
+        this.attributeToken = attribToken;
+        this.methodToken = methodToken;
+    }
+
+    public String getLanguageName(){
+        return this.languageName;
+    }
+
+    public void setLanguageName(String langName){
+        this.languageName = langName;
+    }
+
+    public String getLanguageFileExtension(){
+        return this.languageFileExtension;
+    }
+
+    public void setLanguageFileExtension(String languageFileExtension){
+        this.languageFileExtension = languageFileExtension;
+    }
+
+    public String getClassToken(){
+        return this.classToken;
+    }
+
+    public void setClassToken(String classToken){
+        this.classToken = classToken;
+    }
+
+    public String getAttributeToken(){
+        return this.attributeToken;
+    }
+
+    public void setAttributeToken(String attributeToken){
+        this.attributeToken = attributeToken;
+    }
+
+    public String getMethodToken(){
+        return this.methodToken;
+    }
+
+    public void setMethodToken(String methodToken){
+        this.methodToken = methodToken;
+    }
+}
