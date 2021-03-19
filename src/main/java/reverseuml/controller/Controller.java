db0c359553249086d2fee2b08c04d29b746dc97f
Richast <richard.pluta.12@gmail.com>
2021-03-19 18:57:12 +0000

    Adds initial parsing implementation #9

--- src/main/java/reverseuml/controller/Controller.java
+++ src/main/java/reverseuml/controller/Controller.java
@@ -1,12 +1,16 @@
 package reverseuml.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,21 +22,25 @@ import reverseuml.service.FileService;
 public class Controller {
     @Autowired FileService fileService;
 
+    @ExceptionHandler(IOException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleIOException(IOException e) {}
+
     @PostMapping("/single")
     public void generateClassesFromSingleFile(@RequestParam("file") MultipartFile file) {
         
     }
 
     @PostMapping("/multiple")
-    public List<Class> generateClassesFromMultipleFiles(@RequestParam("file") List<MultipartFile> files) {
+    public List<Class> generateClassesFromMultipleFiles(@RequestParam("file") List<MultipartFile> files) throws IOException {
         ArrayList<Class> classes = new ArrayList<>();
 
-        files.forEach(file -> {
+        for (MultipartFile file : files) {
             Class customClass = fileService.generateClassFromFile(file);
 
             if (customClass != null)
                 classes.add(customClass);
-        });
+        }
 
         return classes;
     }
--- src/main/java/reverseuml/service/FileService.java
+++ src/main/java/reverseuml/service/FileService.java
@@ -2,7 +2,17 @@ package reverseuml.service;
 
 import reverseuml.model.Class;
 
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.springframework.web.multipart.MultipartFile;
+
+import antlr.Java8Lexer;
+import antlr.Java8Parser;
 
 public class FileService {
     public String getFileType(MultipartFile file) {
@@ -10,13 +20,21 @@ public class FileService {
         return filenameParts[filenameParts.length - 1];
     }
 
-    public Class generateClassFromFile(MultipartFile file) {
+    public Class generateClassFromFile(MultipartFile file) throws IOException {
         Class customClass = null;
         String fileType = getFileType(file);
             
         switch(fileType) {
             case "java":
-                // Send file to java parsing class
+                Java8Lexer lexer = new Java8Lexer((CharStream) file.getInputStream());
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                Java8Parser parser = new Java8Parser(tokens);
+                
+                ParseTree tree = parser.compilationUnit();
+                ParseTreeWalker walker = new ParseTreeWalker();
+
+                
+
                 break;
             case "js":
                 // Send file to js parsing class
