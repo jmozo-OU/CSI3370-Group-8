9279fa767cfb4130470358f4c3c5ce0c18b81375
Richast <richard.pluta.12@gmail.com>
2021-04-10 17:52:34 +0000

    Adds initial file uploading

--- src/main/react/public/upload.js
+++ src/main/react/public/upload.js
@@ -1,30 +1,43 @@
-// Run this file and run the index html
-// the file upload is showing in the log
+var multipleUploadForm = document.querySelector('#multipleUploadForm');
+var multipleFileUploadInput = document.querySelector('#multipleFileUploadInput');
+var multipleFileUploadError = document.querySelector('#multipleFileUploadError');
+var multipleFileUploadSuccess = document.querySelector('#multipleFileUploadSuccess');
 
-var express = require('express');
-var app = express();
-var formidable = require('formidable');
+function uploadMultipleFiles(files) {
+    var formData = new FormData();
+    for(var index = 0; index < files.length; index++) {
+        formData.append("files", files[index]);
+    }
 
-app.set('port', process.env.PORT || 3000);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080/reverse-uml/v1/multiple");
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        var response = JSON.parse(xhr.responseText);
+        if(xhr.status == 200) {
+            multipleFileUploadError.style.display = "none";
+            var content = "<p>All Files Uploaded Successfully</p>";
+            for(var i = 0; i < response.length; i++) {
+                content += "<p>DownloadUrl : <a href='" + response[i].fileDownloadUri + "' target='_blank'>" + response[i].fileDownloadUri + "</a></p>";
+            }
+            multipleFileUploadSuccess.innerHTML = content;
+            multipleFileUploadSuccess.style.display = "block";
+        } else {
+            multipleFileUploadSuccess.style.display = "none";
+            multipleFileUploadError.innerHTML = (response && response.message) || "Some Error Occurred";
+        }
+    }
 
-app.post('/multiple', function(req, res) {
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
+    xhr.send(formData);
+}
 
-		console.log('handling form upload - fields', fields);
-		
-		console.log('handling form upload - files', files);
-		
-	});
-	res.send('Thank you');
-});
-
-app.listen(app.get('port'), function() {
-	console.log('Express running on http://localhost:' + app.get('port'));
-});
+multipleUploadForm.addEventListener('submit', function(event){
+    var files = multipleFileUploadInput.files;
+    if(files.length === 0) {
+        multipleFileUploadError.innerHTML = "Please select at least one file";
+        multipleFileUploadError.style.display = "block";
+    }
+    uploadMultipleFiles(files);
+    event.preventDefault();
+}, true);
